e7779fee0601175679157c812eca5d495994f0ab
shubham12101 <shubham12101@iiitd.ac.in>
2015-10-26 21:00:56 +0530

    final midsem commit

--- scripts/training.m
+++ scripts/training.m
@@ -3,6 +3,8 @@ clc
 
 % ===========CONSTANTS===========
 TRAIN_DATA_FILE = '../processed_data/features.csv';
+TEST_DATA_FILE = '../processed_data/test_features.csv';
+TEST_LABEL_FILE = '../processed_data/unlabled.review_labels';
 
 % =========READ DATA============
 trainData = csvread(TRAIN_DATA_FILE);
@@ -10,6 +12,9 @@ trainLabels = zeros(7914,1);
 for i = 1:4000
     trainLabels(i) = trainLabels(i) + 1;
 end
+
+testData = csvread(TEST_DATA_FILE);
+testLabels = importdata(TEST_LABEL_FILE);
 
 % ====== TRAIN & TEST SVM MODEL ========
 % models = cell(6,1);
@@ -20,14 +25,33 @@ end
 %     models{i} = svmtrain(trainData, trainLabels, svmOptions);
 % end
 
-bestcv = 0;
-for log2c = -1:3,
-    cmd = ['-v 5 -c ', num2str(2^log2c)];
-    cv = svmtrain(trainLabels, trainData, cmd);
-    if (cv >= bestcv),
-      bestcv = cv; 
-      bestc = 2^log2c;
-    end
-    fprintf('%g %g (best c=%g, rate=%g)\n', log2c, cv, bestc, bestcv);
-end
+% bestcv = 0;
+% for log2c = -1:3,
+%     cmd = ['-v 5 -c ', num2str(2^log2c)];
+%     cv = svmtrain(trainLabels, trainData, cmd);
+%     if (cv >= bestcv),
+%       bestcv = cv; 
+%       bestc = 2^log2c;
+%     end
+%     fprintf('%g %g (best c=%g, rate=%g)\n', log2c, cv, bestc, bestcv);
+% end
 
+cmd = '-s 0 -t 0 -c 8';
+model = svmtrain(trainLabels, trainData, cmd);
+
+[predictLabels, accuracy, decValues] = svmpredict(testLabels, testData, model);
+CMat = confusionmat(testLabels, predictLabels); 
+
+% bestcv = 0;
+% for log2c = -1:3,
+%   for log2g = -4:1,
+%     cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+%     cv = svmtrain(trainLabels, trainData, cmd);
+%     if (cv >= bestcv),
+%       bestcv = cv;
+%       bestc = 2^log2c;
+%       bestg = 2^log2g;
+%     end
+%     fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
+%   end
+% end
